bc46e655c33ed13448c7d87e32fcb2a87e65f485
Muizaddy <muizaddy@gmail.com>
2024-09-09 13:28:49 +0100

    Edited

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 const hamburger = document.querySelector('.hamburger')
 const lists = document.querySelector('.lists')
 const del = document.querySelector('.delete')
+const allsections = document.querySelectorAll('.section')
+
+
+
 hamburger.addEventListener('click', function () {
   lists.style.display = 'flex'
   lists.style.opacity = 1;
@@ -18,4 +22,25 @@ del.addEventListener('click', function(){
    del.style.display = 'none'
    hamburger.style.display = 'block';
 })
+
+// const revealSection = function(entries, observer){
+// const [entry] = entries
+// console.log(entry)
+// if(!entry.isIntersecting) return 
+// entry.target.classList.remove('section-hidden')
+// observer.unobserve(entry.target)
+// }
+
+
+
+// const sectionObserver = new IntersectionObserver(revealSection, {
+//   root: null,
+//   threshold: 0.15,
+// });
+
+// allsections.forEach(section => { sectionObserver.observe(section)
+// section.classList.add('section-hidden')
+
+// }
+// )
 
